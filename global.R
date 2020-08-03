6ac29155a844fe2eb9eeb2a074a148c9d2e884c4
tennere <elma.tenner@rivm.nl>
2020-08-03 12:30:09 +0200

    Aangepast: andere symbolen voor de knmi data

--- global.R
+++ global.R
@@ -26,7 +26,7 @@ library(purrr)
 library(sp)
 library(devtools)
 library(geoshaper)
-
+library(Rmisc)
 library(DT) #for downlaod and datatable
 library(taRifx)
 library(ggplot2)
@@ -46,7 +46,6 @@ sensor_file <- "Voorbeeld_HLL_sensoren.csv"
 lml_file <- "Voorbeeld_HLL_LML.csv" 
 knmi_file <- "Voorbeeld_HLL_KNMI.csv" 
 
-
 choices <- c( "PM10 - gekalibreerd", "PM2.5 - gekalibreerd","PM10", "PM2.5") #set up choices for shiny app
 kleur_cat <- list('#42145f','#ffb612','#a90061','#777c00','#007bc7','#673327','#e17000','#39870c', '#94710a','#01689b','#f9e11e','#76d2b6','#d52b1e','#8fcae7','#ca005d','#275937','#f092cd')
 kleur_sensor <- "leeg"
@@ -56,11 +55,11 @@ lijn_cat <- list(2,3,4,5,6) # linetype: “blank”, “solid”, “dashed”, 
 
 lijn_stat <- "leeg"
 icons_stations <- iconList(
-  knmi_black = makeIcon("symbol_knmi_black_triangle.svg"),
+  knmi_black = makeIcon("symbol_knmi_black.svg"),
   lml_white = makeIcon("symbol_lml_white.svg"),
-  knmi_white = makeIcon("symbol_knmi_white_triangle.svg"),
+  knmi_white = makeIcon("symbol_knmi_white.svg"),
   lml_black = makeIcon("symbol_lml_black.svg"),
-  knmi_grey = makeIcon("symbol_knmi_grey_triangle.svg"),
+  knmi_grey = makeIcon("symbol_knmi_grey.svg"),
   lml_grey = makeIcon("symbol_lml_grey.svg"))
 
 input_df <- NULL
@@ -75,6 +74,7 @@ knmi_stations_all <- readRDS('locaties_knmi_all.RDS')
 # hier alleen die van de API van het KNMI meenemen
 nummers_knmi <- c(391,370,331,315,324,375,380,240,286,310,283,280,273,323,249,377,316,313,277,348,308,319,215,278,285,343,225,330,267,269,344,275,235,257,290,350,251,248,279,258,356,209,312,340,260,270,242)
 knmi_stations_all$station_nummer <- as.numeric(gsub('knmi_06', '', knmi_stations_all$statcode))
+knmi_stations_all$statcode <- paste0("KNMI_", knmi_stations_all$station_nummer)
 # Neem alleen de stations die in de API van het KNMI zitten
 knmi_stations_all <- knmi_stations_all[which(knmi_stations_all$station_nummer %in% nummers_knmi),]
 knmi_stations_all$selected <- FALSE
@@ -84,6 +84,8 @@ knmi_labels <- as.list(paste("KNMI", knmi_stations_all$station_nummer, sep = ": 
 
 
 # Voor de lml stations: ophalen hun naam en locatie en labels opzetten
+# TODO: dit zijn niet allemaal LML stations, eigenlijk via andere api call alles ophalen
+# TODO: De stationsnamen en nummers en dan per station de locatie...en eigenaar 
 lml_stations_all <- readRDS('locaties_lml_all.RDS')
 lml_stations_all$selected <- FALSE
 lml_stations_all$hasdata <- FALSE
